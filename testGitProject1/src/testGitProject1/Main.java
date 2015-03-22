275433f03ffe0ab83145d65c8d0b3021299465fe
tom <tom>
2015-03-22 17:36:45 +0100

    Initial commit

---
+++ testGitProject1/src/testGitProject1/Main.java
@@ -0,0 +1,10 @@
+package testGitProject1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Default Message");
+	}
+
+}

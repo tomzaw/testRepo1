dd0bbf4b02ce9082205d86ac0e828d374d04d51b
xRazielx <razjel2@gmail.com>
2015-04-02 15:54:44 +0200

    test

--- testGitProject1/src/testGitProject1/Main.java
+++ testGitProject1/src/testGitProject1/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Default Message");
 		
-		System.out.println("Default Message 3");
+		System.out.println("Default Message 4");
 	}
 
 }
